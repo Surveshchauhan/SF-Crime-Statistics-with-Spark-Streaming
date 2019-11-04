e9d693dbbf1925b1e1b2e424efd909b553339f3c
Survesh Chauhan <Surveshchauhan@users.noreply.github.com>
2019-11-04 23:07:41 +0530

    Add files via upload

--- data_stream.py
+++ data_stream.py
@@ -2,7 +2,7 @@ import logging
 import json
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-import pyspark.sql.functions as psf
+import pyspark.sql.functions as sqlf
 from dateutil.parser import parse as parse_date
 
 
@@ -28,8 +28,8 @@ schema = StructType([
 # TODO create a spark udf to convert time to YYYYmmDDhh format
 @psf.udf(StringType())
 def udf_convert_time(timestamp):
-    date_time = parse_date(timestamp)
-    return str(date_time.strftime('%y%y%d%H'))
+    date = parse_date(timestamp)
+    return str(date.strftime('%y%m%d%H'))
 
 
 def run_spark_job(spark):
@@ -48,51 +48,31 @@ def run_spark_job(spark):
         .option("maxOffsetPerTrigger", 10) \
         .load()
     
-
-    # Show schema for the incoming resources for checks
     df.printSchema()
 
-    # TODO extract the correct column from the kafka input resources
-    # Take only value and convert it to String
     kafka_df = df.selectExpr("CAST (value AS STRING)")
 
-    service_table = kafka_df\
-        .select(psf.from_json(psf.col('value'), schema).alias("SERVICE_CALLS"))\
+    service_data_table = kafka_df\
+        .select(psf.from_json(sqlf.col('value'), schema).alias("SERVICE_CALLS"))\
         .select("SERVICE_CALLS.*")
 
-    distinct_table = service_table\
-        .select(psf.col('crime_id'),
-                psf.col('original_crime_type_name'),
-                psf.to_timestamp(psf.col('call_date_time')).alias('call_datetime'),
-                psf.col('address'),
-                psf.col('disposition'))
+    distinct_datatable = service_data_table\
+        .select(sqlf.col('crime_id'),
+                sqlf.col('original_crime_type_name'),
+                sqlf.to_timestamp(sqlf.col('call_date_time')).alias('call_datetime'),
+                sqlf.col('address'),
+                sqlf.col('disposition'))
 
-    # TODO get different types of original_crime_type_name in 60 minutes interval
-    counts_df = distinct_table\
+    
+    c_df = distinct_datatable\
         .withWatermark("original_crime_type_name", "60 minutes")\
-        .groupBy(psf.window(distinct_table.call_datetime,
+        .groupBy(sqlf.window(distinct_datatable.call_datetime,
                             "10 minutes", 
                             "10 minutes"),
-                 distinct_table.original_crime_type_name)\
+                 distinct_datatable.original_crime_type_name)\
                  .count()
 
-    # TODO use udf to convert timestamp to right format on a call_date_time column
-    converted_df = counts_df\
-        .withColumn("call_datetime", udf_convert_time(counts_df.call_datetime))
-
-
-    # TODO apply aggregations using windows function to see how many calls occurred in 2 day span
-    calls_per_2_days = converted_df \
-            .groupBy(psf.window(converted_df.call_date_time, "2 day"))\
-            .agg(psf.count("crime_id").alias("calls_per_2_day"))\
-            .select("calls_per_2_day")
-
-    # TODO write output stream
-    query = service_table\
-        .writeStream\
-        .outputMode("append")\
-        .format("console")\
-        .start()
+    query = c_df.writeStream.outputMode('Complete').format('console').start()
 
     # TODO attach a ProgressReporter
     query.awaitTermination()
